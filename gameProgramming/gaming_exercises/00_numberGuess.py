767e103ebb407dfc9b1646f56304ad23a9cdddd4
JAMAALSMITH <s11588571@duvalschools.org>
2023-09-28 15:00:30 -0400

    9/28
    dasd

--- gameProgramming/gaming_exercises/00_numberGuess.py
+++ gameProgramming/gaming_exercises/00_numberGuess.py
@@ -24,8 +24,8 @@ cpuScore = 0
 numGuesses = 0 
 playerName = "" 
 difficulty = ""
-rangeMin = -1
-rangeMax = -1
+rangeMin = 0
+rangeMax = 30
 numAttempts = -1 
 
 print("""
@@ -45,6 +45,7 @@ print(secretNumber)
 # GAME LOOP
 print("You need to guess a number from 0 to 30. You have four guesses.\n")
 
+
 # ADD CODE HERE TO CHANGE DIFFICULTY BETWEEN EACH ROUND
 # print() an explanation of your three difficulty levels
 # Use input() to store difficulty in difficulty variable.
@@ -58,6 +59,29 @@ while playerScore != 3 and cpuScore != 3: # START THE MATCH (GAME)
     # Whenever you assin a specific value to something, it's called "hard coded".
     #print (secretNumber)
     # ADD CODE HERE TO CHANGE DIFFICULTY BETWEEN EACH ROUND.
+    # Difficulty levels go 1-3
+    difficulty = input("What difficulty would you like to select?")
+    if difficulty == "Easy":
+        print("This is easy mode, you have 2 guesses to guess a number 1-10")
+        numGuesses = 2
+        rangeMin = 1
+        rangeMax = 10
+    elif difficulty == "Medium":
+        print("This is medium mode, you have 5 guesses for numbers 1-20") 
+        numGuesses = 5
+        rangeMin = 1 
+        rangeMax = 20
+    elif difficulty == "Hard":
+        print("This is hard mode, you have 4 guesses to guess a number 1-30")
+        numGuesses = 4
+        rangeMin = 1
+        rangeMax = 30 
+    else: 
+        print("You didn't submit an available level, were you going to pick medium by default?")
+        numGuesses = 5 
+        rangeMin = 1
+        rangeMax = 20 
+    secretNumber = random.randint(rangeMin, rangeMax)
 
     numGuesses = 0
     for guesses in range(4): # START THE ROUND!
