e0ea6fbcea909168435b531cb89b858d79be22c0
JAMAALSMITHII <142834057+JAMAALSMITHII@users.noreply.github.com>
2023-10-31 14:19:57 -0400

    10/31

---
+++ 01_StudentCode/8b/04b_hangman/hangman.py
@@ -0,0 +1,42 @@
+# Hangman Game by Jamaal Smith II, v0.0
+
+words = 'run ran rock roll lean drank vamp black opium purple blurple golden destroy autumn wafflestomper '.split()
+
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = [' ' ' 
+    +---+
+        |
+        |
+        |
+    ======== ''', '''
+    +---+
+    o   |
+        |
+        |
+    ======== ''', '''
+    +---+
+    o   |
+    |   |
+        |
+    ======== ''', '''
+    +---+
+    o   |
+   /|   |
+        |
+    ======== ''', '''
+    +---+
+    o   |
+   /|\  |
+        |
+    ======== ''', '''
+    +---+
+    o   |
+   /|\  |
+   /    |
+    ======== ''', '''
+    +---+
+    o   |
+   /|\  |
+   / \  |
+    ======== ''', ''']
+
