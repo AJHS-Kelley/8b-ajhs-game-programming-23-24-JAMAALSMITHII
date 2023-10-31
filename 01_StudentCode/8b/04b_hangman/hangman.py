143aba224b1a3dde9b63d0f523af8e047e7b2b3b
JAMAALSMITHII <142834057+JAMAALSMITHII@users.noreply.github.com>
2023-10-31 14:51:03 -0400

    10/31

--- 01_StudentCode/8b/04b_hangman/hangman.py
+++ 01_StudentCode/8b/04b_hangman/hangman.py
@@ -1,9 +1,9 @@
 # Hangman Game by Jamaal Smith II, v0.0
-
+import random
 words = 'run ran rock roll lean drank vamp black opium purple blurple golden destroy autumn wafflestomper '.split()
 
 # VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
-HANGMAN_BOARD = [' ' ' 
+HANGMAN_BOARD = [' '' 
     +---+
         |
         |
@@ -40,3 +40,18 @@ HANGMAN_BOARD = [' ' '
    / \  |
     ======== ''', ''']
 
+# Pick Word from List
+def getRandomWord(wordList): # Return a random word from the list.
+    wordIndex = random.randint(0, len(wordlist) - 1)
+    # len(listNmae) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS
+    return wordList[wordIndex]
+
+def display(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end = ' ')
+    for eachLetter in missedLetters:
+        print(eachLetter, end = ' ')
+    print()
+    # FINISH THURSDAY
