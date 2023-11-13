4034781d24e4c56348a61f3aeb5d06b74e1a6272
JAMAALSMITHII <s11588571@students.duvalschools.org>
2023-11-13 14:45:04 -0500

    11/13

---
+++ 01_StudentCode/8b/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -0,0 +1,17 @@
+# Example Game Functions Project, Ryan Kelley, v0,0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3): 
+    pass
+
+def shootBall(shotContested, shotRelease)
+    if shotContested = yes and shotRelease = perfect
