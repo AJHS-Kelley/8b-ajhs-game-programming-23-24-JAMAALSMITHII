1054b0555021fff30b92c616863940efb8c53049
JAMAALSMITHII <s11588571@students.duvalschools.org>
2023-12-08 14:53:11 -0500

    12/8

--- 01_StudentCode/8b/5a_exampleGameFunctions/exampleGameFunctions.py
+++ 01_StudentCode/8b/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,7 +1,7 @@
 # Example Game Functions Project, Ryan Kelley, v0,4
 import random
 
-
+random.randint
 
 # shotContested is the defender contesting your shot. Depending on their defensive level they could either make your shot harder to make or make no difference at all.
 # Having a good shotRelease will help you make more of your shots the higher your shotRelease is the more likely you are of making the shot. A 100 shotRelease will score no matter the level of the defenders contest.
@@ -16,6 +16,9 @@ def shootBall(shotContested, shotRelease):
         return shotBlocked
     return shotMade
 
+shootBall()
+
+
 # You are playing defense and trying to take the ball away from the dribbler/opponent.
 # Inability in stealing the ball will result in a reaching foul on you, the defender.
 def stealBall(speed, strength):
@@ -28,6 +31,9 @@ def stealBall(speed, strength):
         foulBall = True
         return foulBall
     return ballStole
+
+stealBall()
+
 
 def dunkBall(vertical, strength):
     if vertical <= 75 and strength <= 4.0:
@@ -45,16 +51,12 @@ dunkBall(50, 5.0)
 
 
 def jumpBall(vertical):
-    if vertical <= 80:
+    if vertical > 80:
         ballWon = True # Swap with Line 48
         print("WIN JUMPBALL MESSAGE HERE")
-    elif vertical > 80:
+    elif vertical <= 80:
         ballWon = False # Swap with Line 46 
-    else:
-        print('Ball lost! Get on defense! \n')
-        ballLost = True
-        return ballLost
-    return ballWon
+    
     # Greater than 80 = win the jump ball.  if vertical > 80
     # At least 50 but less than 80 = 50% chance to win the jump ball.  elif vertical > 50 
     # Less than 50 = lose the jump ball. else:  
