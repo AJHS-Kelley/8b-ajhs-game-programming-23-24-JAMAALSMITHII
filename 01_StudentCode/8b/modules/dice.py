1ce648bd5c881d214541a1568d4d2e58587c5962
JAMAALSMITHII <s11588571@students.duvalschools.org>
2023-12-14 13:56:46 -0500

    12/14

---
+++ 01_StudentCode/8b/modules/dice.py
@@ -0,0 +1,29 @@
+#Dice Rolling Module by Jamaal Smith II, v0.0
+import random
+
+def roll(numDice, SizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        numRolled += 1
+    return sum
+
+def display(numDice, sizeDice): #Verified Working 12-14-23
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"sum: {sum}\n")
+        numRolled += 1
+    return sum
+    
+def isDoubles(roll1, roll2):
+    if roll1 == roll2:
+        isDoubles = True
+    else:
+        isDoubles = False
+    return isDoubles
---
+++ 01_StudentCode/8b/modules/tester.py
@@ -0,0 +1,10 @@
+import dice
+
+
+roll1 = dice.display(1, 6)
+roll2 = dice.display(1, 6)
+
+if dice.isDoubles(roll1, roll2):
+    print("You rolled a double, go again! \n")
+else:
+    print("It was not a double. \n")
