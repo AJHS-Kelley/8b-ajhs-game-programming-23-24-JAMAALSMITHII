49d5c2c386dca8bff3291906e5c5b2a1a11c4dba
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-01-30 14:14:38 -0500

    1/30

--- 01_StudentCode/8b/ultimate pygame/pygame.py
+++ 01_StudentCode/8b/ultimate pygame/pygame.py
@@ -2,3 +2,12 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+
+while True:
+    for evenet in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
+    
