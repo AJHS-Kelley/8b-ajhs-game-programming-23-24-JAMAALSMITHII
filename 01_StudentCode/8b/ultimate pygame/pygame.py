1dff28c13e15428671d1e2aa9ac51c5d1f6fc6e5
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-02-01 14:47:06 -0500

    2/1

--- 01_StudentCode/8b/ultimate pygame/pygame.py
+++ 01_StudentCode/8b/ultimate pygame/pygame.py
@@ -5,11 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.timeClock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.tff', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Green')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
+
+small_surface = pygame.image.load('graphics.snail/snail1.png').convert()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -17,9 +20,12 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(ground_surface,(0,0))
-    screen.blit(sky_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    # screen/blit(sky_surface, (0,0))
+    # screen/blit(ground_surface,(0,300))
+    # screen/blit(text_surface,(300,50)
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
 
     pygame.display.update()
     clock.tick(60) 
