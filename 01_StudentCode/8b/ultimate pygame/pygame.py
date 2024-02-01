892b21599619308ea8d5adb9dcdd4a6fbe5bdd58
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-02-01 14:26:26 -0500

    2.1

--- 01_StudentCode/8b/ultimate pygame/pygame.py
+++ 01_StudentCode/8b/ultimate pygame/pygame.py
@@ -5,14 +5,21 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.timeClock()
+test_font = pygame.font.Font(None, 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Green')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update everything
+
+    screen.blit(ground_surface,(0,0))
+    screen.blit(sky_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
     pygame.display.update()
-    clock.tick(60)
-    
+    clock.tick(60) 
