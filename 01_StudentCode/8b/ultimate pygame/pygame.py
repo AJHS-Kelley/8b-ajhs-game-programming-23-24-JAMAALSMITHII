b2abd0910cc309aedccdeb793565ec20e407e773
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-02-01 13:58:10 -0500

    2/1

--- 01_StudentCode/8b/ultimate pygame/pygame.py
+++ 01_StudentCode/8b/ultimate pygame/pygame.py
@@ -3,13 +3,16 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock = pygame.timeClock()
 
 while True:
-    for evenet in pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-exit()
+            exit()
     # draw all our elements
     # update everything
     pygame.display.update()
+    clock.tick(60)
     
