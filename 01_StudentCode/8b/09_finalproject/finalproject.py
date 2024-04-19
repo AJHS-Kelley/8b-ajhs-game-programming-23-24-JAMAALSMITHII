65ed3cfcaed42b729a92942784046efab94e7e9f
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-04-19 13:54:52 -0400

    4/19

--- 01_StudentCode/8b/09_finalproject/finalproject.py
+++ 01_StudentCode/8b/09_finalproject/finalproject.py
@@ -31,3 +31,12 @@ res = (720,720)
 # opens up a window  
 screen = pygame.display.set_mode(res)  
   
+# white color  
+color = (255,255,255)  
+  
+# light shade of the button  
+color_light = (170,170,170)  
+  
+# dark shade of the button  
+color_dark = (100,100,100)  
+
