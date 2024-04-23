5e473ea0cc8ecb0ba8869ccd4c7b8e36b1714ce6
JAMAALSMITHII <s11588571@students.duvalschools.org>
2024-04-23 13:49:01 -0400

    4/23

---
+++ 01_StudentCode/8b/09_finalproject/tryExceptFinally.py/tryExceptFinally.py
@@ -0,0 +1,19 @@
+#This is a method for testing code and preventing crashes.
+# try -- except -- else -- finally 
+
+try: # The code in this block is ALWAYS executed.
+    myVariable = 1
+    print:(myVariable)
+    myString = "Five"
+    print(float(myString))
+except NameError: # This code will run IF there is an error (exception) raid
+    print("There is an incorrect cariable name in your code.")
+except: 
+    print("Something else went wrong.")
+else: # This code runs if there are NO ERRORS
+    print("Code executed correctly with no exceptions. \n")
+finally: # THIS CODE ALWAYS RUNS, IT'S LIKE THE TERMINATOR
+    print("I'll be back. \n")
+
+
+#
